0aa6f7157d90e1cad5ab16aa5dab98768fa3ea3a
Eleonora Caponio <eleonora.caponio@gmail.com>
2020-04-29 10:47:21 +0200

    Datetime implementation and traslation features

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from time import time
-import jwt
 from hashlib import md5
-from app import db, login, app
+from time import time
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from app import app, db, login
 
 
 followers = db.Table(
@@ -62,8 +62,9 @@ class User(UserMixin, db.Model):
         return followed.union(own).order_by(Post.timestamp.desc())
 
     def get_reset_password_token(self, expires_in=600):
-        return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in},
-        app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
@@ -88,7 +89,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-
-
-
